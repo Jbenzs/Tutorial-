ea111ebc721c8d20ed81fc1d8d62de3f329d806e
Benzs Jean Francois <benzsjeanfrancois@Benzss-MacBook-Air.local>
2017-01-07 22:53:18 -0500

    created tableview and UI layout

---
+++ LearnMusic/Video.m
@@ -0,0 +1,13 @@
+//
+//  Video.m
+//  LearnMusic
+//
+//  Created by Benzs Jean Francois on 1/7/17.
+//  Copyright © 2017 benzsjeanfrancois. All rights reserved.
+//
+
+#import "Video.h"
+
+@implementation Video
+
+@end
---
+++ LearnMusic/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  LearnMusic
+//
+//  Created by Benzs Jean Francois on 1/7/17.
+//  Copyright © 2017 benzsjeanfrancois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
